e4f1e4b6fda2bdb9d93919c5e35a57df0a5ba137
H. Teng <hevlhayt@foxmail.com>
2016-06-20 14:58:45 +0800

    update 620

--- config.py
+++ config.py
@@ -5,12 +5,11 @@
 # Time: 10:13
 #
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder='static')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12345678@127.0.0.1/seed?charset=utf8'
-app.config['SQLALCHEMY_POOL_RECYCLE'] = 1800
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
-db.create_all()
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12345678@127.0.0.1/seed?charset=utf8'
+# app.config['SQLALCHEMY_POOL_RECYCLE'] = 1800
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# db = SQLAlchemy(app)
+# db.create_all()
--- home.py
+++ home.py
@@ -1,14 +1,10 @@
 # coding: utf-8
 import codecs
-import datetime
 
-from flask import Flask, render_template, send_from_directory
-from config import app, db
-from models import MarkDown, Log
 import misaka as m
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, send_from_directory
 
-
+from config import app
 
 
 @app.route('/static/images/md/<path>')
@@ -18,40 +14,41 @@ def send_md_img(path):
 
 @app.route('/')
 def seed():
-    try:
-        log = Log(timestamp=datetime.datetime.today())
-        db.session.add(log)
-        db.session.commit()
-
-        return render_template('index.html')
-    except Exception, e:
-        print 'Seed Exception: ', e
-    finally:
-        print 'Seed'
-
-
-@app.route('/cat')
-def cat():
-    return render_template('cat.html')
+    # try:
+    #     log = Log(timestamp=datetime.datetime.today())
+    #     db.session.add(log)
+    #     db.session.commit()
+    #
+    #     return render_template('index.html')
+    # except Exception, e:
+    #     print 'Seed Exception: ', e
+    # finally:
+    #     print 'Seed'
+    return render_template('index.html')
 
 
-@app.route('/memo')
-def memo():
-    memos = db.session.query(MarkDown).filter_by(cat='memo').all()
-    # print memos
-    return render_template('cat2.html', title='Memo', cats=memos)
-
-
-@app.route('/acg')
-def acg():
-    acgs = db.session.query(MarkDown).filter_by(cat='acg').all()
-    return render_template('cat2.html', title='ACG', cats=acgs)
-
-
-@app.route('/piece')
-def piece():
-    pieces = db.session.query(MarkDown).filter_by(cat='piece').all()
-    return render_template('cat2.html', title='Piece', cats=pieces)
+# @app.route('/cat')
+# def cat():
+#     return render_template('cat.html')
+#
+#
+# @app.route('/memo')
+# def memo():
+#     memos = db.session.query(MarkDown).filter_by(cat='memo').all()
+#     # print memos
+#     return render_template('cat2.html', title='Memo', cats=memos)
+#
+#
+# @app.route('/acg')
+# def acg():
+#     acgs = db.session.query(MarkDown).filter_by(cat='acg').all()
+#     return render_template('cat2.html', title='ACG', cats=acgs)
+#
+#
+# @app.route('/piece')
+# def piece():
+#     pieces = db.session.query(MarkDown).filter_by(cat='piece').all()
+#     return render_template('cat2.html', title='Piece', cats=pieces)
 
 
 @app.route('/resume')
@@ -61,12 +58,12 @@ def resume():
     res = m.html(text)
     return render_template('blank.html', html=res)
 
-
-@app.route('/md/<id>')
-def md(id=None):
-    html = m.html(MarkDown.query.filter_by(id=id).one().content, extensions=('fenced-code',))
-
-    return render_template('blank.html', html=html)
+#
+# @app.route('/md/<id>')
+# def md(id=None):
+#     html = m.html(MarkDown.query.filter_by(id=id).one().content, extensions=('fenced-code',))
+#
+#     return render_template('blank.html', html=html)
 
 
 if __name__ == '__main__':
